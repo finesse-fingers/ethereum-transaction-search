52aa253e436e04664cd11a769da816dcb803ae2f
bkoteski <bkoteski88@gmail.com>
2020-06-20 14:55:51 +1000

    add input form

---
+++ src/WebApp/Models/EhthereumTransactionSearchRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
+{
+    public class EhthereumTransactionSearchRequest
+    {
+        [Required]
+        public string Block { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+    }
+}
